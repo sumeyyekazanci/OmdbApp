9b87469107c097cbbc27bf6500aa31f5fb3f6d53
Smyy <sumeyyekazanci13@gmail.com>
2022-11-01 23:36:28 +0300

    Initial Commit

---
+++ OmdbApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OmdbApp
+//
+//  Created by Sümeyye Kazancı on 1.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
